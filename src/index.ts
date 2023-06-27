8e2753b8c8dd2b3960153da419a2c67c46ff209a
fabnguess <fabnguess@gmail.com>
2023-06-27 20:06:15 +0000

    feat: connexion du bot au serveur discord

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 import * as dotenv from 'dotenv'
+import { Client, GatewayIntentBits  } from 'discord.js'
 
 dotenv.config();
+
+const KToken = process.env.TOKEN
+
+const bot = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent
+] })
+
+
+bot.on('ready', () => {
+    console.log(`Connecté en tant que ${bot.user?.tag}`);
+  });
+
+bot.login(KToken)
